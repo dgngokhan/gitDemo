16d05bc379bcaa013cbd8940c588901037b9d4cd
dgngokhan <dogangokhan@yahoo.com>
2020-08-01 10:35:59 -0600

    update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class HappyB20 {
+    public static void main(String[] args) {
+        System.out.println("hi");
+        System.out.println("there");
+
+
+
+
+        //gdddd
+    }
 }
